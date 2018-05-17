f08311458748a9971a5b6bc87ef630df68a37e9d
杨扬 <jianhuyy@hotmail.com>
2018-05-17 21:11:07 +0800

    Now you can specify the size of the image block.

--- MyMosaic/main.cpp
+++ MyMosaic/main.cpp
@@ -49,6 +49,7 @@ int main(int argc, char** argv)
 	clock_t start = clock();
 
 	std::string input_path = sanitize_path(argv[1]);
+	int size = std::stoi(argv[3]);
 	//std::string dir = input_path + "*.*";
 
 	WIN32_FIND_DATA data;
@@ -68,8 +69,8 @@ int main(int argc, char** argv)
 		std::string img_path = input_path + "/" + data.cFileName;
 
 		cv::Mat img = cv::imread(img_path);
-		cv::Size size(20, 20);
-		cv::resize(img, img, size);
+		cv::Size img_size(size, size);
+		cv::resize(img, img, img_size);
 		//cv::imshow("", img);
 		//cv::imwrite(std::to_string(num_images) + ".jpg", img);
 		//cv::waitKey(0);
@@ -80,19 +81,19 @@ int main(int argc, char** argv)
 		int green_sum = 0;
 		int red_sum = 0;
 
-		for (int i = 0; i < img.rows; i++)
+		for (int i = 0; i < size; i++)
 		{
 			uchar* data = img.ptr<uchar>(i);
-			for (int j = 0; j < img.cols; j++)
+			for (int j = 0; j < size; j++)
 			{
 				blue_sum += data[j * 3];
 				green_sum += data[j * 3 + 1];
 				red_sum += data[j * 3 + 2];
 			}
 		}
-		my_data[num_images][0] = static_cast<float>(blue_sum) / (img.rows * img.cols);
-		my_data[num_images][1] = static_cast<float>(green_sum) / (img.rows * img.cols);
-		my_data[num_images][2] = static_cast<float>(red_sum) / (img.rows * img.cols);
+		my_data[num_images][0] = static_cast<float>(blue_sum) / (size * size);
+		my_data[num_images][1] = static_cast<float>(green_sum) / (size * size);
+		my_data[num_images][2] = static_cast<float>(red_sum) / (size * size);
 
 
 		num_images++;
@@ -109,27 +110,27 @@ int main(int argc, char** argv)
 	cv::Mat target = cv::imread(argv[2]);
 	std::vector<int> index_list;
 	//cv::imshow("", target);
-	for (int i = 0; i < target.rows / 20; i++)
+	for (int i = 0; i < target.rows / size; i++)
 	{
-		for (int j = 0; j < target.cols / 20; j++)
+		for (int j = 0; j < target.cols / size; j++)
 		{
-			cv::Mat roi = target(cv::Rect(j * 20, i * 20, 20, 20));
+			cv::Mat roi = target(cv::Rect(j * size, i * size, size, size));
 			int blue_sum = 0;
 			int green_sum = 0;
 			int red_sum = 0;
-			for (int m = 0; m < 20; m++)
+			for (int m = 0; m < size; m++)
 			{
 				uchar* data = roi.ptr<uchar>(m);
-				for (int n = 0; n < 20; n++)
+				for (int n = 0; n < size; n++)
 				{
 					blue_sum += data[n * 3];
 					green_sum += data[n * 3 + 1];
 					red_sum += data[n * 3 + 2];
 				}
 			}
-			float blue_aver = blue_sum / (20.0f * 20.0f);
-			float green_aver = green_sum / (20.0f * 20.0f);
-			float red_aver = red_sum / (20.0f * 20.0f);
+			float blue_aver = static_cast<float>(blue_sum) / (size * size);
+			float green_aver = static_cast<float>(green_sum) / (size * size);
+			float red_aver = static_cast<float>(red_sum) / (size * size);
 			std::vector<float> qv = { blue_aver,green_aver,red_aver };
 			kdTree::kdTreeResultVector result;
 			my_tree->NNearestAroundPoint(qv, 1, result);
@@ -143,9 +144,9 @@ int main(int argc, char** argv)
 		//    x = i % 72 
 		//    y = i / 72
 		//std::cout << i << std::endl;
-		int x = i % (target.cols / 20);
-		int y = i / (target.cols / 20);
-		cv::Rect rect(x * 20, y * 20, 20, 20);
+		int x = i % (target.cols / size);
+		int y = i / (target.cols / size);
+		cv::Rect rect(x * size, y * size, size, size);
 		img_list[index_list[i]].copyTo(result(rect));
 	}
 
--- README.md
+++ README.md
@@ -6,3 +6,11 @@ This program is used to achieve this photo effect.
 # Dependencies
 OpenCV
 Boost
+
+# Parameters setting
+There are three command line parameters you need to specify.
+The first is the folder in where you put your image database.
+The second is the target image.
+The third is the size of the image block.
+e.g.   
+C:\Users\jianh\Desktop\py\results C:\Users\jianh\Desktop\mmexport1526359945110.jpg 5
